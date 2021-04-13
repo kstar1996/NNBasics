e744921d58b3bf443b7ec6052e9296b1db8f36c4
eujin <kstar1996@gmail.com>
2021-04-13 10:41:45 +0900

    Change in loss function (cross entropy)

--- loss_function.py
+++ loss_function.py
@@ -10,5 +10,10 @@ def sum_squares_error(y, t):
 
 # cross entropy error, 교차 엔트로피
 def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
     delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
