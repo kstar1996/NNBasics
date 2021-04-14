972d9bcd8c34c4071c8e492d3895d2531dfa2946
eujin <kstar1996@gmail.com>
2021-04-14 11:27:46 +0900

    Added to gradient.py

--- differentiation.py
+++ differentiation.py
@@ -22,7 +22,7 @@ def numerical_gradient(f, x):
         fxh2 = f(x)
 
         grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val # 값 복원
+        x[idx] = tmp_val  # 값 복원
 
     return grad
 
--- gradient.py
+++ gradient.py
@@ -1,5 +1,7 @@
 import numpy as np
 from differentiation import numerical_gradient
+from softmax import softmax
+from loss_function import cross_entropy_error
 
 
 # lr is learning rate
@@ -11,3 +13,24 @@ def gradient_descent(f, init_x, lr=0.01, step_num=100):
         grad = numerical_gradient(f, x)
         x -= lr*grad
     return x
+
+
+class SimpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)  # 정규분포로 초기화
+
+    # x is input data and t is the answer label
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+
+# net = SimpleNet()
+# 가중치 매개변
+# print(net.W)
