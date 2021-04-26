8dc6795e859a5129d7c15cabff5b1ce3c3123aad
eujin <kstar1996@gmail.com>
2021-04-26 14:49:16 +0900

    renamed im2col to convolution.py

--- DeepLearning/convolution.py
+++ DeepLearning/convolution.py
@@ -20,3 +20,22 @@ def im2col(input_data, filter_h, filter_w, stride=1, padding=0):
 
     col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
     return col
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1+ (H + 2*self.pad - FH) / self.stride)
+        out_w = int(1+ (W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x)
+
+
+
+
